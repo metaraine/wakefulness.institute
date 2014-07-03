390b76a1959cac822c84dfff4c76fd9cba34576a
metaraine <raineorshine@gmail.com>
2014-07-03 07:38:27 -0600

    Add favicon

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require('express')
+var bodyParser = require('body-parser')
+var favicon = require('serve-favicon')
 
-var app = express();
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+var app = express()
+app.set('view engine', 'jade')
+app.set('views', __dirname + '/views')
+app.use(express.static(__dirname + '/public'))
+app.use(favicon(__dirname + '/public/favicon.ico'))
 app.use(bodyParser());
 
 app.get('/', function(req, res) {
 	res.render('index');
-});
+})
 
 var server = app.listen(8419, function() {
 	console.log('Express server listening on port ' + server.address().port);
-});
+})
